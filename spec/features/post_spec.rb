9f56ee6eddff581c3fcd2a2c8a32046d39cf061a
Joshua Ard <ard.josh@gmail.com>
2018-05-09 21:39:13 -0400

    add new posts form

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,38 @@ describe "navigate" do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe "creation" do
+    before do
+      user = User.create!(
+        first_name: "Jon",
+        last_name: "Snow",
+        email: "test@test.com",
+        password: "asdfasdf",
+        password_confirmation: "asdfasdf",
+      )
+      login_as(user, :scope => :user)
+      visit new_post_path
+    end
+
+    it "has a new form that can be reached" do
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be created from new form page" do
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "Some rationale"
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
+    end
+
+    it "will have a user associated with it" do
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "User_Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User_Association")
+    end
+  end
 end
