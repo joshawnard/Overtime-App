2ffc89e150c663bcdd7c7c4881472820bb5403a5
Joshua Ard <ard.josh@gmail.com>
2018-05-09 21:06:50 -0400

    add seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |n|
+  Post.create!(
+    date: Date.today,
+    rationale: "Rationale #{n}",
+  )
+end
+
+puts "100 posts created"
